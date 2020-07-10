dd39c5023c6a3a701cf5081e7cc99f6155c5f224
Orell Garten <orell.garten@rub.de>
2020-07-10 16:08:50 +0200

    split cache options in extra file

---
+++ cmake/cache_options.cmake
@@ -0,0 +1,5 @@
+option(MYPROJECT_VAR1 "Boolen project variable" ON)
+SET(MYPROJECT_VAR2 "" CACHE STRING "string project variable")
+
+OPTION(BUILD_TEST "Build test cases" ON)
+OPTION(BUILD_BENCHMARK "Build benchmarks cases" OFF)

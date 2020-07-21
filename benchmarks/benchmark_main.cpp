af161b6df5133acf883a281fe983fd902fea64b1
Orell Garten <orell.garten@rub.de>
2020-07-21 14:56:38 +0200

    benchmarks

--- benchmarks/benchmark_main.cpp
+++ benchmarks/benchmark_main.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include <string>
 
 int main(int argc, char **argv) {
 
@@ -12,3 +14,21 @@ int main(int argc, char **argv) {
 
   benchmark::RunSpecifiedBenchmarks();
 }
+
+static void BM_myProject(benchmark::State &state) {
+  /*
+   * init sequence
+   */
+  std::string s{};
+  for (auto _ : state) {
+    /*
+     * actual stuff to benchmark
+     */
+
+    s += "fuck yeah ";
+  }
+  /*
+   * tear down
+   */
+}
+BENCHMARK(BM_myProject)->DenseRange(2, 4, 1)->Unit(benchmark::kMillisecond);
--- cmake/cache_options.cmake
+++ cmake/cache_options.cmake
@@ -1,5 +1,5 @@
-option(MYPROJECT_VAR1 "Boolen project variable" ON)
+OPTION(MYPROJECT_VAR1 "Boolen project variable" ON)
 SET(MYPROJECT_VAR2 "" CACHE STRING "string project variable")
 
 OPTION(BUILD_TEST "Build test cases" ON)
-OPTION(BUILD_BENCHMARK "Build benchmarks cases" OFF)
+OPTION(BUILD_BENCHMARK "Build benchmarks cases" ON)

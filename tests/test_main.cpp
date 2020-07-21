cd0b00431b90810c04dc315e5957c1d405875f78
Orell Garten <orell.garten@rub.de>
2020-07-21 14:56:25 +0200

    examplary test file

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,8 @@
-int main() {
-  return 0;
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("test1") {
+  REQUIRE(std::sqrt(1) == 1);
+  REQUIRE_FALSE(2 == 1);
 }
